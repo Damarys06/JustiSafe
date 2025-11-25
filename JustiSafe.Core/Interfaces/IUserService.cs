4b23bc8cc861489956646aea4ccdd3bb85471872
Juan Proaño <juan.proano02@epn.edu.ec>
2025-11-24 19:05:54 -0500

    chore: nombre y apellido para generar el usuario

--- JustiSafe.Core/Interfaces/IUserService.cs
+++ JustiSafe.Core/Interfaces/IUserService.cs
@@ -1,11 +1,13 @@
-﻿using JustiSafe.Data.Entities;
+using JustiSafe.Data.Entities;
 using System.Threading.Tasks;
 
 namespace JustiSafe.Core.Interfaces
 {
     public interface IUserService
     {
-        Task<User> RegisterUser(string username, string password, string role);
+        // Ahora recibimos Nombre y Apellido para generar el usuario internamente
+        Task<User> RegisterUser(string firstName, string lastName, string password, string role);
+
         Task<User?> Login(string username, string password);
     }
 }
